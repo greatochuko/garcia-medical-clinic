67067cab66bf4b9e03967a3dc883831610c98db0
Great Ogheneochuko <greatochuko123@gmail.com>
2025-10-20 08:58:43 +0100

    remove logging from pusher config

--- resources/js/lib/pusherClient.js
+++ resources/js/lib/pusherClient.js
@@ -1,7 +1,5 @@
 import Pusher from "pusher-js";
 
-Pusher.logToConsole = true;
-
 const pusher = new Pusher(import.meta.env.VITE_PUSHER_APP_KEY, {
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     forceTLS: true,
